e10d982deec64e110e2ab294979d9c24c18b22a8
shiningliang <lsnboost@outlook.com>
2019-05-19 23:52:15 +0800

    add parallel segmentation

--- pretrain_embedding.py
+++ pretrain_embedding.py
@@ -6,9 +6,12 @@ import matplotlib.pyplot as plt
 import multiprocessing
 from gensim.models import word2vec
 from gensim.models.word2vec import PathLineSentences
+import logging
+from tqdm import tqdm
 
 
 raw_file = './data/raw/text.txt'
+seg_file = './data/raw/seg_text.txt'
 clean_file = './data/raw/clean_text.txt'
 
 
@@ -38,35 +41,53 @@ def clean_func(line, stopwords):
     return [word for word in line if word not in stopwords]
 
 
-def clean_txt(in_path, out_path):
-    with open(in_path, 'r', encoding='utf8') as fin:
-        raw_lines = fin.readlines()
+def clean_txt(in_path, seg_path, out_path):
+    logger = logging.getLogger('Cleaning')
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    # logger.info('Loading raw text')
+    # with open(in_path, 'r', encoding='utf8') as fin:
+    #     raw_lines = fin.readlines()
+    # fin.close()
+    #
+    logger.info('Segment raw text')
+    # seg = pkuseg.pkuseg()
+    # seg_lines = [seg.cut(line) for line in raw_lines]
+
+    # pkuseg.test(in_path, seg_path, nthread=multiprocessing.cpu_count())
+    logger.info('Loading seg text')
+    with open(seg_path, 'r', encoding='utf8') as fin:
+        seg_lines = fin.readlines()
     fin.close()
 
-    seg = pkuseg.pkuseg()
-    seg_lines = [seg.cut(line) for line in raw_lines]
+    seg_lines = [line.strip().split() for line in seg_lines]
     raw_len = [len(line) for line in seg_lines]
-    print('Rows of raw text - ', len(seg_lines))
+    logger.info('Rows of raw text - {}'.format(len(seg_lines)))
+    stat(raw_len, 'raw')
 
     with open('./data/raw/stopwords.txt', 'r', encoding='utf8') as fs:
         stopwords = fs.readlines()
     fs.close()
     stopwords = [word.strip() for word in stopwords]
 
-    pool = multiprocessing.Pool(processes=12)
+    logger.info('Removing stopwords')
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
     results = []
     for i, line in enumerate(seg_lines):
         results.append(pool.apply_async(clean_func, (line, stopwords,)))
     pool.close()
     pool.join()
     clean_lines = [res.get() for res in results]
-    print('Rows of clean text - ', len(clean_lines))
+    logger.info('Rows of clean text - {}'.format(len(clean_lines)))
     clean_lines = filter(lambda x: len(x) > 2, clean_lines)
     clean_lines = list(clean_lines)
     clean_len = [len(line) for line in clean_lines]
-    print('Rows of clean text - ', len(clean_lines))
-
-    stat(raw_len, 'raw')
+    logger.info('Rows of clean text - {}'.format(len(clean_lines)))
     stat(clean_len, 'clean')
 
     with open(out_path, 'w', encoding='utf8') as fout:
@@ -77,4 +98,4 @@ def clean_txt(in_path, out_path):
     print('hello world')
 
 
-clean_txt(raw_file, clean_file)
+clean_txt(raw_file, seg_file, clean_file)
