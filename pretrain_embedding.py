7f5c1d4589589d8fdfe5f67a3d5d7f15fb8246ce
shiningliang <lsnboost@outlook.com>
2019-05-20 21:47:08 +0800

    add train word embeddings

--- pretrain_embedding.py
+++ pretrain_embedding.py
@@ -1,4 +1,7 @@
 import pkuseg
+import re
+import string
+from zhon.hanzi import punctuation
 import numpy as np
 import pandas as pd
 from scipy import stats
@@ -7,6 +10,8 @@ import multiprocessing
 from gensim.models import word2vec
 from gensim.models.word2vec import PathLineSentences
 import logging
+import os
+import sys
 from tqdm import tqdm
 
 
@@ -37,11 +42,11 @@ def stat(seq_length, type):
     plt.savefig(type + '_len_stats.jpg', format='jpg')
 
 
-def clean_func(line, stopwords):
-    return [word for word in line if word not in stopwords]
+def clean_func(line):
+    return re.sub(r"[%s]+" % string.punctuation, "", re.sub(r"[%s]+" % punctuation, "", line))
 
 
-def clean_txt(in_path, seg_path, out_path):
+def clean_txt(in_path, seg_path, out_path, is_clean=True, is_seg=True):
     logger = logging.getLogger('Cleaning')
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -50,52 +55,63 @@ def clean_txt(in_path, seg_path, out_path):
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
-    # logger.info('Loading raw text')
-    # with open(in_path, 'r', encoding='utf8') as fin:
-    #     raw_lines = fin.readlines()
-    # fin.close()
-    #
-    logger.info('Segment raw text')
-    # seg = pkuseg.pkuseg()
-    # seg_lines = [seg.cut(line) for line in raw_lines]
+    if is_clean:
+        logger.info('Loading raw text')
+        with open(in_path, 'r', encoding='utf8') as fin:
+            raw_lines = fin.readlines()
+        fin.close()
 
-    # pkuseg.test(in_path, seg_path, nthread=multiprocessing.cpu_count())
-    logger.info('Loading seg text')
-    with open(seg_path, 'r', encoding='utf8') as fin:
-        seg_lines = fin.readlines()
-    fin.close()
+        logger.info('Removing punctuations')
+        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+        results = []
+        for i, line in enumerate(raw_lines):
+            results.append(pool.apply_async(clean_func, (line,)))
+        pool.close()
+        pool.join()
+        clean_lines = [res.get() for res in results]
+        with open(out_path, 'w', encoding='utf8') as fout:
+            for line in clean_lines:
+                fout.writelines(line)
+        fout.close()
 
-    seg_lines = [line.strip().split() for line in seg_lines]
-    raw_len = [len(line) for line in seg_lines]
-    logger.info('Rows of raw text - {}'.format(len(seg_lines)))
-    stat(raw_len, 'raw')
+    if is_seg:
+        logger.info('Segmenting clean text')
+        # seg = pkuseg.pkuseg()
+        # seg_lines = [seg.cut(line) for line in raw_lines]
 
-    with open('./data/raw/stopwords.txt', 'r', encoding='utf8') as fs:
-        stopwords = fs.readlines()
-    fs.close()
-    stopwords = [word.strip() for word in stopwords]
+        pkuseg.test(out_path, seg_path, nthread=multiprocessing.cpu_count())
+        logger.info('Loading segmented text')
+        with open(seg_path, 'r', encoding='utf8') as fin:
+            seg_lines = fin.readlines()
+        fin.close()
 
-    logger.info('Removing stopwords')
-    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-    results = []
-    for i, line in enumerate(seg_lines):
-        results.append(pool.apply_async(clean_func, (line, stopwords,)))
-    pool.close()
-    pool.join()
-    clean_lines = [res.get() for res in results]
-    logger.info('Rows of clean text - {}'.format(len(clean_lines)))
-    clean_lines = filter(lambda x: len(x) > 2, clean_lines)
-    clean_lines = list(clean_lines)
-    clean_len = [len(line) for line in clean_lines]
-    logger.info('Rows of clean text - {}'.format(len(clean_lines)))
-    stat(clean_len, 'clean')
+        seg_lines = [line.strip().split() for line in seg_lines]
+        seg_len = [len(line) for line in seg_lines]
+        logger.info('Rows of segmented text - {}'.format(len(seg_lines)))
+        stat(seg_len, 'segmented')
 
-    with open(out_path, 'w', encoding='utf8') as fout:
-        for line in clean_lines:
-            fout.writelines(' '.join(line) + '\n')
-    fout.close()
+        clean_lines = filter(lambda x: len(x) > 2, seg_lines)
+        clean_lines = list(clean_lines)
+        clean_len = [len(line) for line in clean_lines]
+        logger.info('Rows of filtered text - {}'.format(len(clean_lines)))
+        stat(clean_len, 'filtered')
+
+        with open(seg_path, 'w', encoding='utf8') as fout:
+            for line in clean_lines:
+                fout.writelines(' '.join(line) + '\n')
+        fout.close()
 
     print('hello world')
 
 
-clean_txt(raw_file, seg_file, clean_file)
+if __name__ == '__main__':
+    # clean_txt(raw_file, seg_file, clean_file)
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
+
+    logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+    model = word2vec.Word2Vec(PathLineSentences(seg_file), sg=1, size=300, window=5, min_count=10, sample=1e-4,
+                              workers=multiprocessing.cpu_count())
+    model.wv.save_word2vec_format('./data/processed/word2vec.txt', binary=False)
